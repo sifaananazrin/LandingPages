b90dcfc69fd75648eb41446b4c5075b28c36d38f
Shifana nasrin <shifananazrin15@gmail.com>
2023-09-01 13:26:57 +0530

    Bugs fixed

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>  <div className="w-[1544px] left-[188px] top-[32px] absolute justify-start items-start gap-20 inline-flex">
+    <div className="grow shrink basis-0 h-20 justify-start items-center gap-20 flex">
+      <div className="p-5 bg-violet-50 rounded-[99px] justify-center items-center flex">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="18"
+          viewBox="0 0 16 18"
+          fill="none"
+        >
+          <path
+            id="a"
+            d="M14.1644 0C14.6947 0 15.1332 0.183785 15.4799 0.551353C15.8266 0.918919 16 1.39459 16 1.97838V15.6973C16 16.2595 15.8266 16.7351 15.4799 17.1243C15.1332 17.4919 14.6947 17.6757 14.1644 17.6757C13.6342 17.6757 13.1957 17.4919 12.8489 17.1243C12.5226 16.7568 12.3493 16.2919 12.3289 15.7297C11.819 16.3568 11.1256 16.8973 10.2486 17.3514C9.39197 17.7838 8.48439 18 7.52581 18C6.11855 18 4.84385 17.6216 3.70172 16.8649C2.55959 16.0865 1.65201 15.0162 0.978968 13.6541C0.326323 12.2919 0 10.7459 0 9.01622C0 7.28649 0.326323 5.74054 0.978968 4.37838C1.63161 2.99459 2.5188 1.92432 3.64054 1.16757C4.78266 0.38919 6.03697 0 7.40344 0C8.38241 0 9.30019 0.194595 10.1568 0.583784C11.0134 0.951351 11.7374 1.42703 12.3289 2.01081V1.97838C12.3289 1.41621 12.5022 0.951351 12.8489 0.583784C13.1957 0.194595 13.6342 0 14.1644 0ZM7.9847 14.4324C9.31039 14.4324 10.3913 13.9243 11.2275 12.9081C12.0637 11.8703 12.4818 10.573 12.4818 9.01622C12.4818 7.45946 12.0637 6.16216 11.2275 5.12432C10.3913 4.08649 9.31039 3.56757 7.9847 3.56757C6.67941 3.56757 5.60867 4.08649 4.77247 5.12432C3.93626 6.16216 3.51816 7.45946 3.51816 9.01622C3.51816 10.573 3.92607 11.8703 4.74187 12.9081C5.57808 13.9243 6.65902 14.4324 7.9847 14.4324Z"
+            fill="#5243C2"
+          />
+        </svg>
+      </div>
+      <div className="grow shrink basis-0 h-12 justify-start items-start gap-[59px] flex">
+        <div className="flex-col justify-start items-center gap-3 inline-flex">
+          <div className="pt-3.5 justify-start items-start gap-1 inline-flex">
+            <div className="text-gray-800 text-base font-normal leading-tight">
+              Discover
+            </div>
+          </div>
+          <div className="w-[27px] h-0.5 relative rounded-[99px]" />
+        </div>
+        <div className="flex-col justify-start items-center gap-3 inline-flex">
+          <div className="pt-3.5 justify-start items-start gap-1 inline-flex">
+            <div className="text-gray-800 text-base font-semibold leading-tight">
+              Make Your Burger
+            </div>
+          </div>
+          <div className="w-[140px] h-0.5 relative bg-indigo-700 rounded-[99px]" />
+        </div>
+      </div>
+    </div>
+    <div className="py-[11px] justify-start items-center gap-[60px] flex">
+      <div className="bg-rose-50 rounded-[99px] justify-center items-center flex">
+        <div className="px-3 py-1.5 justify-center items-center gap-1 flex">
+          <div className="w-4 h-4 justify-center items-center flex">
+            <div className="w-4 h-4 relative"></div>
+          </div>
+          <div className="text-red-500 text-base font-semibold leading-tight">
+            Call Me Back
+          </div>
+        </div>
+      </div>
+      <div className="text-gray-800 text-base font-semibold leading-tight">
+        8 800 437-87-22
+      </div>
+      <div className="w-[58px] h-[58px] justify-center items-center flex">
+        <div className="p-3.5 bg-violet-50 rounded-[99px] flex-col justify-start items-start inline-flex">
+          <div className="w-[30px] h-[30px] pl-0.5 pr-[3px] py-0.5 justify-center items-center inline-flex">
+            <div className="w-[25px] h-[26px] relative">
+              <img
+                className="w-6 h-6 left-0 top-0 absolute"
+                src="https://s3-alpha-sig.figma.com/img/2d49/e706/59574e9e376940d47ef68df808b95016?Expires=1694390400&Signature=dv-PuI3RpE6UeHe59xTA-bdmtM86zXkun~PsmONb3sz9~E8LsOEEZ7cXstbKh5NJtkku-nteijlg7O8fUPMWTcW8~C~L8n3MFzuPLEaxqIVgszdF0buFxtoATxdPopvTQvzzoI9XvNHAdZT0t0XT6XmA5Mk1aFDTpC0EhztTTKKBVWpE122aCmBAcdYmdB2kYezdzIWbnU14WeME21xHJPSTO0h04g0WimL8BgJcxy692VKFTtAiw~U7CUROqa32j1MRosZDjxySFhUmdg7BrOzfYOTpuRF-ax0HjaESwh8sYB7kqtT93vdLvzSyx4aZXMbeECSt4HhyML6WZ625UQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              />
+              <div className="w-2.5 h-2.5 left-[15px] top-[16px] absolute rounded-[999px] border border-slate-100">
+                <div className="w-2.5 h-[5px] left-0 top-0 absolute bg-blue-600" />
+                <div className="w-2.5 h-[5px] left-0 top-[5px] absolute bg-yellow-400" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> 
+
+    </>
+  )
+}
+
+export default Header
